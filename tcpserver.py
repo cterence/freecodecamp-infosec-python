bb5648dadbb1479258e94e2bf2eed187c892414f
Terence Chateigne <terence.chateigne@posteo.net>
2020-11-01 21:58:04 +0100

    created tcp server

---
+++ tcpserver.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import socket
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = socket.gethostname()
+port = 8888
+
+serversocket.bind((host, port))
+
+serversocket.listen(3)
+
+while True:
+    clientsocket, address = serversocket.accept()
+
+    print(f"Received connexion from {str(address)}")
+
+    message = "Welcome ! Thank you for connecting to the server.\r\n"
+
+    clientsocket.send(message.encode("ascii"))
+
+    clientsocket.close()
